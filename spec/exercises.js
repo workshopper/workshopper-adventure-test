37147b03032a841f998b1df547b25aade63cd51a
César Carruitero <ccarruitero@protonmail.com>
2019-08-05 21:01:09 -0500

    allow use config file to customize where find exercises for verify

--- spec/exercises.js
+++ spec/exercises.js
@@ -3,6 +3,12 @@ var assert = require('assert')
 var path = require('path')
 var _ = require('lodash')
 var wUtil = require('workshopper-adventure/util')
+var config
+try {
+  config = require(`${process.cwd()}/.workshopper-test.config.js`)
+} catch (_) {
+  config = {}
+}
 
 function getExercises (done) {
   exec.async(['list'], function (err, stdout, stderr) {
@@ -44,23 +50,30 @@ describe('Testing exercises', function () {
   var exercises = exec.sync(['list', '--lang=en']).toString().split('\n')
   exercises.pop() // last line break
   exercises.map(function (exercise) {
-    return wUtil.idFromName(exercise)
-  }).forEach(function (id, nr) {
-    var folder = path.join(process.cwd(), 'test', id)
-    var allFiles = require('glob').sync('*.*', {
+    return {
+      name: exercise,
+      id: wUtil.idFromName(exercise, config.spaceChar)
+    }
+  }).forEach(function ({ name, id }, nr) {
+    var folder = path.join(process.cwd(), config.exercisesFolder || 'test', id)
+    var allFiles = require('glob').sync(config.files || '*.*', {
       cwd: folder
     })
 
     allFiles.filter(function (file) {
-      return /^(in)?valid(-|_)?(\d*)?\.\w+/.test(file)
+      var valid = config.validRegex || /^(in)?valid(-|_)?(\d*)?\.\w+/
+      var validRe = new RegExp(valid)
+      return validRe.test(file)
     }).forEach(function (file, fileNr) {
       it('./' + path.relative(process.cwd(), path.join(folder, file)) + ' (' + nr + ':' + fileNr + ')\t ', function (done) {
-        exec.async(['select', id], function (err, stdout, stderr) {
+        exec.async(['select', name, '--lang=en'], function (err, stdout, stderr) {
           if (err) {
             throw new Error('Select didnt work out: ' + err)
           }
           exec.async(['verify', path.resolve(folder, file)], function (err, stdout2, stdrr) {
-            if (/^invalid/.test(file)) {
+            var invalid = config.invalidRegex || /^invalid/
+            var invalidRe = new RegExp(invalid)
+            if (invalidRe.test(file)) {
               if (!err) {
                 throw new Error(stdout2)
               }
